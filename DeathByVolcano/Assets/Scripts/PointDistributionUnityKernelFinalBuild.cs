ad81f8a7242cd224c8f08c55168f5b1fbac4e5b7
Schnugaf <espenonarheim@gmail.com>
2016-01-30 20:22:40 +0100

    TestScene for collision and updated collision scripts.
    Still not working cause I am dumb dumb.

--- DeathByVolcano/Assets/Scripts/PointDistributionUnityKernelFinalBuild.cs
+++ DeathByVolcano/Assets/Scripts/PointDistributionUnityKernelFinalBuild.cs
@@ -9,50 +9,44 @@ public class PointDistributionUnityKernelFinalBuild : MonoBehaviour
     public int PlayerOneScore;
     public int PlayerTwoScore;
     public static PointDistributionUnityKernelFinalBuild InstanceScore;
+    public static WhenTheManComesAroundDynamcs godInst;
+    WhenTheManComesAroundDynamcs GodScript;
+    GameObject God;
 
     // Use this for initialization
     void Start()
     {
         InstanceScore = this;
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        CheckScoreUpdate();
+        God = GameObject.Find("VolcanoGod");
+        GodScript = God.GetComponent<WhenTheManComesAroundDynamcs>();
+
 
     }
 
     void lavaCollisionOrIncinerationIfYouWill(Collision2D lavaCollision)
     {
-        if (lavaCollision.transform.tag == "Player1" || lavaCollision.transform.tag == "Player2")
+        if (lavaCollision.gameObject.tag == "Player1" || lavaCollision.gameObject.tag == "Player2")
         {
-            if (tag == "Player1")
+            if (lavaCollision.gameObject.tag == "Player1")
             {
                 PlayerOneScore = PlayerOneScore + lavaScoreValue;
                 Destroy(GameObject.FindWithTag("Player1"));
             }
 
-            if (tag == "Player2")
+            if (lavaCollision.gameObject.tag == "Player2")
             {
                 PlayerTwoScore = PlayerTwoScore + lavaScoreValue;
                 Destroy(GameObject.FindWithTag("Player2"));
             }
         }
 
-    }
-
-    void CheckScoreUpdate()
-    {
-        if (PlayerOneScore < WhenTheManComesAroundDynamcs.godClass.pOneScore)
-        {
-            PlayerOneScore = WhenTheManComesAroundDynamcs.godClass.pOneScore;
-        }
-
-        if (PlayerTwoScore < WhenTheManComesAroundDynamcs.godClass.pOneScore)
-        {
-            PlayerTwoScore = WhenTheManComesAroundDynamcs.godClass.PTwoScore;
-        }
     }
 
 
