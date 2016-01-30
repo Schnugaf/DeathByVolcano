5a86e25bb9a59a59ee5602598e0a51b08280ef5d
Boothand <boothand@hotmail.com>
2016-01-30 02:59:11 +0100

    Placeholder rig, player can aim and charge a shot.

---
+++ DeathByVolcano/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+	public string vertical;
+	public string charging;
+	[HideInInspector]public bool isCharging;
+	[HideInInspector]public float verticalAxis;
+
+	void Start ()
+	{
+		
+	}
+
+	void Update ()
+	{
+		verticalAxis = Input.GetAxis (vertical);
+		isCharging = Input.GetButton (charging);
+	}
+}
---
+++ DeathByVolcano/Assets/Scripts/PlayerThink.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class PlayerThink : MonoBehaviour
+{
+	PlayerInput pi;
+	public Transform aimParent;
+
+	public float aimSensitivity = 1f;
+	public float maxAimAngle = 70f;
+	public float oppositeAimAngle = 280f;
+
+	public float maxChargeTime = 2f;
+	float chargeTimer;
+	bool startedCharge;
+
+	void Start ()
+	{
+		pi = GetComponent<PlayerInput> ();
+	}
+
+	void Update ()
+	{
+		//Aiming up/down
+		aimParent.localEulerAngles += new Vector3(0f, 0f, pi.verticalAxis * aimSensitivity);
+
+		if (aimParent.localEulerAngles.z > maxAimAngle)
+		{
+			if (aimParent.localEulerAngles.z < oppositeAimAngle && aimParent.localEulerAngles.z > oppositeAimAngle - 10f)
+			{
+				aimParent.localEulerAngles = new Vector3 (0f, 0f, oppositeAimAngle);
+			}
+			else if (aimParent.localEulerAngles.z < maxAimAngle + 10)
+			{
+				aimParent.localEulerAngles = new Vector3 (0f, 0f, maxAimAngle);
+			}
+		}
+
+		//Charging a shot
+		if (pi.isCharging)
+		{
+			startedCharge = true;
+			chargeTimer += Time.deltaTime;
+		}
+
+		//Release
+		if (startedCharge && !pi.isCharging)
+		{
+			startedCharge = false;
+			//Send projectile
+		}
+
+		chargeTimer = Mathf.Clamp (chargeTimer, 0f, maxChargeTime);
+	}
+}
