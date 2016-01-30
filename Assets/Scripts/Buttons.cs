dfbe1aef51cad02806c2d4d0efb8c294fe36c9fc
Xxeron <jtsm12@gmail.com>
2016-01-30 01:03:05 +0100

    Start menu buttons working
    Start Menu buttons works, but no Scenes to load.

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour {
+
+	public void startClicked ()
+	{
+		SceneManager.LoadScene("MainGame");
+	}
+
+	public void tutorialClicked()
+	{
+		SceneManager.LoadScene ("Tutorial");
+	}
+
+	public void exitClicked()
+	{
+		print ("Shit Got Real");
+		Application.Quit();
+
+	}
+}
