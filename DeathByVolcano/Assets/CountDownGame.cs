9ff1fb5fc2440bc2ab7ca827cc2ec614e2c399a3
Schnugaf <espenonarheim@gmail.com>
2016-01-31 07:53:43 +0100

    SHITS ABOUT TO GO DOWN 'YALL
    Added point system proper.

---
+++ DeathByVolcano/Assets/CountDownGame.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CountDownGame : MonoBehaviour
+{
+
+    public float curST;
+    public float endST;
+
+    // Use this for initialization
+    void Start()
+    {
+        CountDownStart();
+        Time.timeScale = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void CountDownStart()
+    {
+        curST += Time.deltaTime;
+        if (curST > endST)
+        {
+            Time.timeScale = 1f;
+            CountDownRound();
+        }
+    }
+
+    void CountDownRound()
+    {
+
+    }
+}
