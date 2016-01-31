54ecf336e55444e0ac07d257799a4d061b01e22a
Schnugaf <espenonarheim@gmail.com>
2016-01-31 11:02:34 +0100

    Updated stuff; Added timer.

--- DeathByVolcano/Assets/CountDownGame.cs
+++ DeathByVolcano/Assets/CountDownGame.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class CountDownGame : MonoBehaviour
@@ -7,31 +8,55 @@ public class CountDownGame : MonoBehaviour
     public float curST;
     public float endST;
 
+    public float curCDR;
+    public float endCDR;
+
+    float tCount;
+    int tCountint;
+    Text visTim;
+
+    GameObject EndGUI;
+    
     // Use this for initialization
     void Start()
     {
-        CountDownStart();
-        Time.timeScale = 0f;
+        curST = 0;
+
+            
     }
 
     // Update is called once per frame
     void Update()
     {
 
-    }
+        EndGUI = GameObject.Find("EndGUI");
 
-    void CountDownStart()
-    {
-        curST += Time.deltaTime;
-        if (curST > endST)
-        {
-            Time.timeScale = 1f;
-            CountDownRound();
-        }
+        CountDownRound();
+
+        visualTimer();
     }
 
     void CountDownRound()
     {
+        curCDR += Time.deltaTime;
+        if (curCDR > endCDR)
+        {
+            print("Time is Out");
+            Time.timeScale = 0;
+            EndGUI.SetActive(true);
+            
 
+        }
+        print(curCDR);
+
+    }
+
+    void visualTimer()
+    {
+        tCount = endCDR - curCDR;
+        tCountint = (int)tCount;
+        
+        visTim = GetComponent<Text>();
+        visTim.text = tCountint.ToString();
     }
 }
